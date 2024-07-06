5e725422c1a26d056180e65b599258bb87756418
juneboku <juneboku@gmail.com>
2024-07-06 23:26:22 +0900

    Top page

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+app.get('/', (c) => {
+  return c.text('api.june29.jp')
+})
+
 app.get('/nikki/:date', (c) => {
   const date = c.req.param('date')
   const newUrl = `https://app.juneboku.xyz/nikki/${date}`
