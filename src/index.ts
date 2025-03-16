bd46d3bf8ea98d220469ed9bcdcc62baed7cdf2d
juneboku <juneboku@gmail.com>
2025-03-16 23:30:08 +0900

    Update destination

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('api.june29.jp')
+  return c.text('NRR')
 })
 
 app.get('/nikki/:date', (c) => {
   const date = c.req.param('date')
-  const newUrl = `https://app.juneboku.xyz/nikki/${date}`
+  const newUrl = `https://jnbk.app/nikki/${date}`
   return c.redirect(newUrl)
 })
 
